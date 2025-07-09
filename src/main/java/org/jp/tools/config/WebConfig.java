5c8fba048b1afa0d6006d93da75b52d90115914c
JP <jothiprakash888@gmail.com>
2025-07-09 15:51:39 +0530

    allow cors origin : ui render

--- src/main/java/org/jp/tools/config/WebConfig.java
+++ src/main/java/org/jp/tools/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // frontend local dev
+                .allowedOrigins("http://localhost:3000",// frontend local dev
+                        "https://tools-ui.onrender.com"   ) // frontend prod from render
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
